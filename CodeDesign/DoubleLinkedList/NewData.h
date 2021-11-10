dc0b748c7df8af57e0c65701093d6956e3597b67
Keegan <KMiller.EilexInteractive@gmail.com>
2021-11-11 10:04:11 +1100

    Added constructor & inherit from the state class

--- CodeDesign/DoubleLinkedList/NewData.h
+++ CodeDesign/DoubleLinkedList/NewData.h
@@ -1,16 +1,17 @@
 #pragma once
-
+#include "State.h"
 enum class InsertType
 {
 	Standard,
 	Front,
 	Back
 };
-class NewData
+class NewData : public State
 {
 public:
-	NewData(InsertType location = InsertType::Standard);
-	~NewData();
+	NewData(InsertType location = InsertType::Standard) { insertLocation = location; }
+	~NewData() {}
+
 
 private:
 	InsertType insertLocation;
